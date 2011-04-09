c1d0a600e0eaaef98f92f98835be8ad66806207a
dusknoir <me@dusknoir.net>
2011-04-08 22:31:41 -0400

    added expiration timer

--- main.lua
+++ main.lua
@@ -3,17 +3,41 @@
 local frame = CreateFrame("Frame", nil, UIParent)
 frame:RegisterEvent("UPDATE_BATTLEFIELD_STATUS")
 
-frame:SetScript("OnEvent", function()
-    for i=1, MAX_BATTLEFIELD_QUEUES do
+local expires = "Expires in: %.1f"
+
+function frame:OnUpdate(delay)
+    self.timer = self.timer - delay
+    self.button:SetText(expires:format(self.timer))
+
+    if self.timer < 0 then
+        self.timer = nil
+        self.button = nil
+        self:SetScript("OnUpdate", nil)
+    end
+end
+
+frame:SetScript("OnEvent", function(self)
+    for i = 1, MAX_BATTLEFIELD_QUEUES do
         local status, _, _, _, _, _, registeredMatch = GetBattlefieldStatus(i)
         if registeredMatch == 1 then
             if status == "confirm" then
+                local btn
+
                 if StaticPopup1Button2:GetText() == "Leave Queue" and StaticPopup1Button2:IsEnabled() then
                     StaticPopup1Button2:Disable()
+                    btn = StaticPopup1Button2
                 end
+
                 if StaticPopup2Button2:GetText() == "Leave Queue" and StaticPopup2Button2:IsEnabled() then 
                     StaticPopup2Button2:Disable()
-                end    
+                    btn = StaticPopup2Button2
+                end
+
+                if btn and not self.timer then
+                    self.timer = 20
+                    self.button = btn
+                    self:SetScript("OnUpdate", self.OnUpdate)
+                end
             end
             break
         end
@@ -21,7 +45,7 @@ frame:SetScript("OnEvent", function()
 end)
 
 if AddonLoader then
-    frame:GetScript("OnEvent")()
+    frame:GetScript("OnEvent")(frame)
 end
 
 -- leave queue when the button is clicked

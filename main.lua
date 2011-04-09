d7bb2ec2e268171dfd06297db9e88940323fbdc3
dusknoir <me@dusknoir.net>
2011-04-09 04:25:05 -0400

    move addonloader stuff to the .toc file; use localized LEAVE_QUEUE; minor cleanup

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
--- disable "leave queue" button
+-- disable "leave queue" button & add expiration timer
 
-local frame = CreateFrame("Frame", nil, UIParent)
+local expires = "Expires in %.1f"
+
+local frame = CreateFrame("Frame", "SecureQueue", UIParent)
 frame:RegisterEvent("UPDATE_BATTLEFIELD_STATUS")
-
-local expires = "Expires in: %.1f"
 
 function frame:OnUpdate(delay)
     self.timer = self.timer - delay
@@ -23,12 +23,12 @@ frame:SetScript("OnEvent", function(self)
             if status == "confirm" then
                 local btn
 
-                if StaticPopup1Button2:GetText() == "Leave Queue" and StaticPopup1Button2:IsEnabled() then
+                if StaticPopup1Button2:GetText() == LEAVE_QUEUE and StaticPopup1Button2:IsEnabled() then
                     StaticPopup1Button2:Disable()
                     btn = StaticPopup1Button2
                 end
 
-                if StaticPopup2Button2:GetText() == "Leave Queue" and StaticPopup2Button2:IsEnabled() then 
+                if StaticPopup2Button2:GetText() == LEAVE_QUEUE and StaticPopup2Button2:IsEnabled() then 
                     StaticPopup2Button2:Disable()
                     btn = StaticPopup2Button2
                 end
@@ -44,15 +44,11 @@ frame:SetScript("OnEvent", function(self)
     end
 end)
 
-if AddonLoader then
-    frame:GetScript("OnEvent")(frame)
-end
-
--- leave queue when the button is clicked
+-- leave queue when the button is clicked (skip the dropdown)
 
 MiniMapBattlefieldFrame:HookScript("OnClick", function(self, btn)
     if btn == "RightButton" then
-        for i=1, MAX_BATTLEFIELD_QUEUES do
+        for i = 1, MAX_BATTLEFIELD_QUEUES do
             local status, _, _, _, _, _, registeredMatch = GetBattlefieldStatus(i)
             if registeredMatch == 1 then
                 if status == "queued" then
